33a6138c11c16c8af94be38fae26fa940ff56eae
Premjeet-yadav <ypremjeet360@gmail.com>
2023-03-17 13:41:56 +0530

    local storage working complete

--- expressjs/mensdesc.js
+++ expressjs/mensdesc.js
@@ -4,7 +4,7 @@ function displayproduct(){
     product_desc.map(function(el){
         var div1 = document.createElement("div");
         var div2 = document.createElement("div");
-        
+
         var img = document.createElement("img");
         img.setAttribute("src",el.img_url);
         img.setAttribute("alt",el.name);
@@ -15,9 +15,43 @@ function displayproduct(){
         name.textContent = el.name;
         var price = document.createElement("p");
         price.textContent = el.price;
+        var btn = document.createElement("button");
+        btn.textContent = "Add To Cart";
+        btn.setAttribute("id","cartbtn");
+        btn.addEventListener("click",function(){
+            addToCart(el);
+        })
+        var select_size = document.createElement("select");
+        select_size.setAttribute("id","size_select");
+        let option1 = document.createElement("option");
+        option1.text = "Select Size";
+        option1.value = "";
+        select_size.appendChild(option1);
+        for(var i=0;i<el.size.length;i++){
+            var option=document.createElement("option");
+            option.setAttribute("value",el.size[i]);
+            option.textContent=el.size[i];
+            select_size.appendChild(option);
+        }
+        select_size.addEventListener("change",function(){
+            changefun(el)
+        });
         div1.append(img);
-        div2.append(desc,name);
+        div2.append(desc,name,price,select_size,btn);
      document.getElementById("parent").append(div1,div2);
     })
     
+}
+
+function changefun(product){
+    var selected = document.getElementById("size_select").value;
+    product["selected"] = selected;
+    console.log(selected);
+}
+
+
+var cart_data = [];
+function addToCart(product){
+    cart_data.push(product);
+    localStorage.setItem("cartData",JSON.stringify(cart_data));
 }
