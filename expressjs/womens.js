f75772b0d444377e2fbe195e338e263ed1c8d9da
Premjeet-yadav <ypremjeet360@gmail.com>
2023-03-18 18:46:26 +0530

    more product page added

---
+++ expressjs/womens.js
@@ -0,0 +1,259 @@
+var womens_data = [
+    {
+        img_url : "https://images.express.com/is/image/expressfashion/0086_06418421_0259_f001?cache=on&wid=361&fmt=jpeg&qlt=75,1&resmode=sharp2&op_usm=1,1,5,0&defaultImage=Photo-Coming-Soon",
+        desc : "NEW | online only",
+        name : "Body Contour High Compression Matte High Neck Cropped Tank",
+        price : 44,
+        size : ['S','M','L','XL','XXL'],
+        color : "Neon Berry 259",
+    },
+    {
+        img_url : "https://images.express.com/is/image/expressfashion/0086_06419035_0309_f001?cache=on&wid=361&fmt=jpeg&qlt=75,1&resmode=sharp2&op_usm=1,1,5,0&defaultImage=Photo-Coming-Soon",
+        desc : "NEW | online only",
+        name : "Body Contour High Compression Matte Scoop Neck Thong Bodysuit",
+        price : 48,
+        size : ['S','M','L','XL','XXL'],
+        color : "Orange Mango 209",
+    },
+    {
+        img_url : "https://images.express.com/is/image/expressfashion/0086_06419676_0001_f001?cache=on&wid=361&fmt=jpeg&qlt=75,1&resmode=sharp2&op_usm=1,1,5,0&defaultImage=Photo-Coming-Soon",
+        desc : "NEW | online only",
+        name : "Body Contour High Compression Matte Square Neck Cropped Tank",
+        price : 44,
+        size : ['S','M','L','XL','XXL'],
+        color : "Daimond White 123",
+    },
+    {
+        img_url : "https://images.express.com/is/image/expressfashion/0097_09718242_0259_f001?cache=on&wid=361&fmt=jpeg&qlt=75,1&resmode=sharp2&op_usm=1,1,5,0&defaultImage=Photo-Coming-Soon",
+        desc : "NEW | online only",
+        name : "Cotton-Blend Boyfriend Portfino Shirt",
+        price : 54,
+        size : ['S','M','L','XL','XXL'],
+        color : "Neon Berry 259",
+    },
+    {
+        img_url : "https://images.express.com/is/image/expressfashion/0086_06340386_0058_f001?cache=on&wid=361&fmt=jpeg&qlt=75,1&resmode=sharp2&op_usm=1,1,5,0&defaultImage=Photo-Coming-Soon",
+        desc : "NEW | online only",
+        name : "Supersoft Fitted Double Layer Crew Neck Tee",
+        price : 34,
+        size : ['S','M','L','XL','XXL'],
+        color : "Jet Black 73",
+    },
+    {
+        img_url : "https://images.express.com/is/image/expressfashion/0097_08639217_0309_f001?cache=on&wid=361&fmt=jpeg&qlt=75,1&resmode=sharp2&op_usm=1,1,5,0&defaultImage=Photo-Coming-Soon",
+        desc : "NEW | online only",
+        name : "Crew Neck Cropped Tank",
+        price : 58,
+        size : ['S','M','L','XL','XXL'],
+        color : "Orange Mango 209",
+    },
+    {
+        img_url : "https://images.express.com/is/image/expressfashion/0086_06340367_0259_f001?cache=on&wid=361&fmt=jpeg&qlt=75,1&resmode=sharp2&op_usm=1,1,5,0&defaultImage=Photo-Coming-Soon",
+        desc : "NEW | online only",
+        name : "Supersoft Relaxed Crew Neck Tee",
+        price : 34,
+        size : ['S','M','L','XL','XXL'],
+        color : "Neon Berry 309",
+    },
+    {
+        img_url : "https://images.express.com/is/image/expressfashion/0097_09707654_2913_f001?cache=on&wid=361&fmt=jpeg&qlt=75,1&resmode=sharp2&op_usm=1,1,5,0&defaultImage=Photo-Coming-Soon",
+        desc : "NEW | online only",
+        name : "Ruffeled Neck Relaxed Portfino Shirt",
+        price : 50,
+        size : ['S','M','L','XL','XXL'],
+        color : "Bubble 2913",
+    },
+    {
+        img_url : "https://images.express.com/is/image/expressfashion/0086_09610586_0121_f001?cache=on&wid=361&fmt=jpeg&qlt=75,1&resmode=sharp2&op_usm=1,1,5,0&defaultImage=Photo-Coming-Soon",
+        desc : "NEW | online only",
+        name : "Body Contour Compression Off The Shoulder Ruched Thong Bodysuit",
+        price : 68,
+        size : ['S','M','L','XL','XXL'],
+        color : "Bubble 2913",
+    },
+    {
+        img_url : "https://images.express.com/is/image/expressfashion/0097_09709440_2757_f001?cache=on&wid=361&fmt=jpeg&qlt=75,1&resmode=sharp2&op_usm=1,1,5,0&defaultImage=Photo-Coming-Soon",
+        desc : "NEW | online only",
+        name : "Tie Front Shirt Thong Bodysuit",
+        price : 68,
+        size : ['S','M','L','XL','XXL'],
+        color : "Bullion 2757",
+    },
+    {
+        img_url : "https://images.express.com/is/image/expressfashion/0097_09709227_0121_f001?cache=on&wid=361&fmt=jpeg&qlt=75,1&resmode=sharp2&op_usm=1,1,5,0&defaultImage=Photo-Coming-Soon",
+        desc : "NEW | online only",
+        name : "Eyelet Lace Tie Neck Puff Sleeve Top",
+        price : 128,
+        size : ['S','M','L','XL','XXL'],
+        color : "Swan 121",
+    },
+    {
+        img_url : "https://images.express.com/is/image/expressfashion/0097_08639176_2757_f001?cache=on&wid=361&fmt=jpeg&qlt=75,1&resmode=sharp2&op_usm=1,1,5,0&defaultImage=Photo-Coming-Soon",
+        desc : "NEW | online only",
+        name : "Satin Scoop Neck Downtown Cami",
+        price : 40,
+        size : ['S','M','L','XL','XXL'],
+        color : "Bullion 2757",
+    },
+    {
+        img_url : "https://images.express.com/is/image/expressfashion/0097_08739455_0017_f001?cache=on&wid=361&fmt=jpeg&qlt=75,1&resmode=sharp2&op_usm=1,1,5,0&defaultImage=Photo-Coming-Soon",
+        desc : "NEW | online only",
+        name : "Floral Short Sleeve Button Front Boxy Shirt",
+        price : 64,
+        size : ['S','M','L','XL','XXL'],
+        color : "Pink Print 17",
+    },
+    {
+        img_url : "https://images.express.com/is/image/expressfashion/0097_08639161_0011_f001?cache=on&wid=361&fmt=jpeg&qlt=75,1&resmode=sharp2&op_usm=1,1,5,0&defaultImage=Photo-Coming-Soon",
+        desc : "NEW | online only",
+        name : "Satin Floral Print Lace Strap V-Neck Cami",
+        price : 44,
+        size : ['S','M','L','XL','XXL'],
+        color : "Black Print 11",
+    },
+    {
+        img_url : "https://images.express.com/is/image/expressfashion/0086_06340767_2915_f001?cache=on&wid=361&fmt=jpeg&qlt=75,1&resmode=sharp2&op_usm=1,1,5,0&defaultImage=Photo-Coming-Soon",
+        desc : "NEW | online only",
+        name : "Supersoft Crew Neck Short Sleeve Thong Bodysuit",
+        price : 44,
+        size : ['S','M','L','XL','XXL'],
+        color : "State Heather Grey 2915",
+    },
+    {
+        img_url : "https://images.express.com/is/image/expressfashion/0097_08659328_0024_f001?cache=on&wid=361&fmt=jpeg&qlt=75,1&resmode=sharp2&op_usm=1,1,5,0&defaultImage=Photo-Coming-Soon",
+        desc : "NEW | online only",
+        name : "Floral Pleated Tie Long Sleeve Top",
+        price : 98,
+        size : ['S','M','L','XL','XXL'],
+        color : "Neutral Print 24",
+    },
+    {
+        img_url : "https://images.express.com/is/image/expressfashion/0097_08659221_0058_f001?cache=on&wid=361&fmt=jpeg&qlt=75,1&resmode=sharp2&op_usm=1,1,5,0&defaultImage=Photo-Coming-Soon",
+        desc : "NEW | online only",
+        name : "V-Neck Puff Sleeve Pleated Peplum Top",
+        price : 74,
+        size : ['S','M','L','XL','XXL'],
+        color : "Pitch Black 58",
+    },
+    {
+        img_url :"https://images.express.com/is/image/expressfashion/0097_08739457_0027_f001?cache=on&wid=361&fmt=jpeg&qlt=75,1&resmode=sharp2&op_usm=1,1,5,0&defaultImage=Photo-Coming-Soon",
+        desc : "NEW | online only",
+        name : "Printed Short Sleeve Button Front Boxy Shirt",
+        price : 64,
+        size : ['S','M','L','XL','XXL'],
+        color : "Purple Print 27",
+    },
+    {
+        img_url : "https://images.express.com/is/image/expressfashion/0096_08263189_0058_f001?cache=on&wid=361&fmt=jpeg&qlt=75,1&resmode=sharp2&op_usm=1,1,5,0&defaultImage=Photo-Coming-Soon",
+        desc : "NEW | online only",
+        name : "Crew Neck Puff Sleeve Lace Sweater",
+        price : 98,
+        size : ['S','M','L','XL','XXL'],
+        color : "Pitch Black 58",
+    },
+    {
+        img_url : "https://images.express.com/is/image/expressfashion/0096_08263164_0121_f001?cache=on&wid=361&fmt=jpeg&qlt=75,1&resmode=sharp2&op_usm=1,1,5,0&defaultImage=Photo-Coming-Soon",
+        desc : "NEW | online only",
+        name : "Ribbed Embellished Button Polo Cardigan",
+        price : 98,
+        size : ['S','M','L','XL','XXL'],
+        color : "Swan 121",
+    },
+    {
+        img_url : "https://images.express.com/is/image/expressfashion/0086_06340419_0058_f001?cache=on&wid=361&fmt=jpeg&qlt=75,1&resmode=sharp2&op_usm=1,1,5,0&defaultImage=Photo-Coming-Soon",
+        desc : "NEW | online only",
+        name : "Skimming Padded Shoulder O-Ring Boxy Tank",
+        price : 58,
+        size : ['S','M','L','XL','XXL'],
+        color : "Pitch Black 58",
+    },
+    {
+        img_url : "https://images.express.com/is/image/expressfashion/0096_08412143_0001_f001?cache=on&wid=361&fmt=jpeg&qlt=75,1&resmode=sharp2&op_usm=1,1,5,0&defaultImage=Photo-Coming-Soon",
+        desc : "NEW | online only",
+        name : "Body Contour Ribbed One Button Cardigan",
+        price : 78,
+        size : ['S','M','L','XL','XXL'],
+        color : "White 1",
+    },
+]
+
+displayproduct(womens_data)
+function displayproduct(womens_data){
+    document.getElementById("parent").innerHTML="";
+    womens_data.map(function(el){
+        var div = document.createElement("div");
+        var img = document.createElement("img");
+        img.setAttribute("src",el.img_url);
+        img.setAttribute("alt",el.name);
+        img.setAttribute("class","product_image");
+        var desc = document.createElement("p");
+        desc.textContent = el.desc;
+        var name = document.createElement("h3");
+        name.textContent = el.name;
+        var price = document.createElement("p");
+        price.textContent = "$"+el.price+".00";
+        div.addEventListener("click",function(){
+            navToNextPage(el);
+        });
+        div.append(img,desc,name,price);
+     document.getElementById("parent").append(div);
+    })
+    
+}
+var product_desc = [];
+function navToNextPage(product){
+    product_desc.push(product);
+    localStorage.setItem("decription_data",JSON.stringify(product_desc));
+    window.location.href="./mensdesc.html";
+}
+document.getElementById("sort").addEventListener("change",pricesort);
+function pricesort(){
+    
+    var selected = document.getElementById("sort").value;
+    if (selected == "HighToLow") {
+        
+    var newarr = womens_data.sort(function(a,b){
+      return parseInt(b.price) - parseInt(a.price);
+    });
+    displayproduct(newarr);
+    }
+
+    if(selected == "lowToHigh"){
+        console.log("helllo");
+    var newarr2 = womens_data.sort(function(a,b){
+      return parseInt(a.price) - parseInt(b.price);
+    });
+    displayproduct(newarr2);
+    }
+    if(selected == ""){
+        displayproduct(womens_data);
+    }
+}
+document.getElementById("price_filter").addEventListener("change",filterbyprice);
+function filterbyprice(){
+    var selected = document.getElementById("price_filter").value;
+    if(selected=="30-50"){
+        var newarr = womens_data.filter(function(el){
+            return (el.price>=30 && el.price<=50)
+
+        })
+        displayproduct(newarr);
+    }
+    if(selected=="50-70"){
+        var newarr = womens_data.filter(function(el){
+            return (el.price>=50 && el.price<=70)
+
+        })
+        displayproduct(newarr);
+    }
+    if(selected=="70-90"){
+        var newarr = womens_data.filter(function(el){
+            return (el.price>=70 && el.price<=90)
+
+        })
+        displayproduct(newarr);
+    }
+    if(selected==""){
+        displayproduct(womens_data);
+    }
+}
